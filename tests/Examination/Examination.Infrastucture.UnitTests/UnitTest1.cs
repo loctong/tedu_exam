6565ca389ed04c74afef7166119bac3fa4ed4b7b
loc.tong <loctong@gmail.com>
2021-07-22 23:33:24 +0700

    2

---
+++ tests/Examination/Examination.Infrastucture.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Examination.Infrastucture.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
