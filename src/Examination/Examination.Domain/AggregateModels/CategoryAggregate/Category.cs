561136ce9dafb21687a51c4f2e0224505cb1d68d
loc.tong <loc.tong@ptnglobalcorp.com>
2021-07-24 21:50:53 +0700

    Add Entity Domain

---
+++ src/Examination/Examination.Domain/AggregateModels/CategoryAggregate/Category.cs
@@ -0,0 +1,13 @@
+using Examination.Domain.SeedWork;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Examination.Domain.AggregateModels.CategoryAggregate
+{
+    public class Category : Entity
+    {
+        [BsonElement("name")]
+        public string Name { set; get; }
+        [BsonElement("urlPath")]
+        public string UrlPath { set; get; } //domain/exam-category-1
+    }
+}
---
+++ src/Examination/Examination.Domain/AggregateModels/ExamAggregate/IExamRepository.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Examination.Domain.SeedWork;
+
+namespace Examination.Domain.AggregateModels.ExamAggregate
+{
+    public interface IExamRepository : IRepositoryBase<Exam>
+    {
+        Task<IEnumerable<Exam>> GetExamListAsync();
+        Task<Exam> GetExamByIdAsync(string id);
+    }
+}
