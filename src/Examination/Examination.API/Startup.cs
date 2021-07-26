b83637277a625c1f119453a3a8e9f5457714fae3
loc.tong <loc.tong@ptnglobalcorp.com>
2021-07-26 20:50:28 +0700

    Add Api #1

--- src/Examination/Examination.API/Startup.cs
+++ src/Examination/Examination.API/Startup.cs
@@ -2,7 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Examination.Application.Mapping;
+using Examination.Domain.AggregateModels.ExamAggregate;
+using Examination.Domain.AggregateModels.ExamResultAggregate;
+using Examination.Domain.AggregateModels.UserAggregate;
+using Examination.Infrastructure.Repositories;
 using Examination.Infrastructure.SeedWork;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,12 +35,31 @@ namespace Examination.API
         {
 
             services.AddControllers();
+            services.AddAutoMapper(cfg => {
+                cfg.AddProfile(new MappingProfile());
+            });
+            services.AddMediatR(typeof(Startup).Assembly);
+
+            services.AddCors(options => 
+            {
+                options.AddPolicy("CorsPolicy",
+                builder => builder
+                .SetIsOriginAllowed((host)  => true)
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+            });
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Examination.API", Version = "v1" });
             });
 
             services.Configure<ExamSettings>(Configuration);
+
+            services.AddTransient<IExamRepository, ExamRepository>();
+            services.AddTransient<IExamResultRepository, ExamResultRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
